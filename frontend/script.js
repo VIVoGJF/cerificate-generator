b7e91bf26890f796dd186a420ada8de3c98273a5
Vivek Mandal <vivek171004@gmail.com>
2026-07-11 06:01:05 +0530

    updated deployed api bas

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://cerificate-generator-37kb.onrender.com";
 
 const downloadBtn = document.getElementById("downloadBtn");
 const input = document.getElementById("donorName");
